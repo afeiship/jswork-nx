5122939dcaeae3ecc36ce0a32b37d0bdd73a7593
afeiship <1290657123@qq.com>
2025-01-14 23:11:19 +0800

    Release 0.0.2

--- jswork-nx/build.gradle.kts
+++ jswork-nx/build.gradle.kts
@@ -7,9 +7,6 @@ java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
-dependencies {
-    implementation(libs.media3.common)
-}
 
 publishing {
     publications {
