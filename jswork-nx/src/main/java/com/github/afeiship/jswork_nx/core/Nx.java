eb95b9e17089aadf9e7d16d7a0dce5c57fef1966
afeiship <1290657123@qq.com>
2025-01-14 22:59:22 +0800

    fix: rm deps

--- jswork-nx/src/main/java/com/github/afeiship/jswork_nx/core/Nx.java
+++ jswork-nx/src/main/java/com/github/afeiship/jswork_nx/core/Nx.java
@@ -1,9 +1,7 @@
 package com.github.afeiship.jswork_nx.core;
 
-import androidx.media3.common.util.Log;
-
 public class Nx {
     public static void log(String message) {
-        Log.d("Nx", message);
+        System.out.println(message);
     }
 }
